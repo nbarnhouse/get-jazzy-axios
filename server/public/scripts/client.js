3a32068d4e8ee1e5ba8bf05f39d91a3d0e933eaf
Nicole Barnhouse <nicolebarnhouse@gmail.com>
2023-12-16 19:33:09 -0600

    working 100% but not styled

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,73 +4,73 @@ function fetchSong() {
     axios({
         method: 'GET',
         url: '/song'
-    })
+        })
         .then(function (response) {
             // Code that will run on successful response
-            // from the server.
             console.log(response);
-            // quotesFromServer will be an Array of quotes
-            let quotesFromServer = response.data;
-            let contentDiv = document.querySelector('#songTableBody');
-            for (let song of quotesFromServer) {
-                contentDiv.innerHTML += `
-                <tr>
-                    <td>${song.title}</td>
-                    <td>${song.artist}</td>
-                </tr>
-            `;
-            }   
-        }).catch(function (error) {
-            // Code that will run on any errors from the server.
-            console.log(error);
-            console.error('ERROR', error)
-        });
-};
-
-function fetchArtist() {
-    //Get request for artist
-    axios({
-        method: 'GET',
-        url: '/artist'
-    })
-        .then(function (response) {
-            // Code that will run on successful response
-            // from the server.
-            console.log(response);
-            // quotesFromServer will be an Array of quotes
-            let quotesFromServer = response.data;
-            let contentDiv = document.querySelector('#artistTableBody');
-            for (let artist of quotesFromServer) {
-                contentDiv.innerHTML += `
-                <tr>
-                    <td>${artist.name}</td>
-                    <td>${artist.born}</td>
-                    <td>${artist.died}</td>
-                </tr>
-            `;
-            }
-        }).catch(function (error) {
+            renderSong(response.data);   
+        })
+        .catch(function (error) {
             // Code that will run on any errors from the server.
             console.log(error);
             console.error('ERROR', error)
         })
 };
 
-// function renderSong() {
-//   //Render song data to the DOM
-// };
+function fetchArtist() {
 
-// function renderArtist() {
-//   //Render artist data to the DOM
-// };
+    //Get request for artist
+    axios({
+        method: 'GET',
+        url: '/artist'
+        })
+        .then(function (response) {
+            // Code that will run on successful response
+            // from the server.
+            console.log(response);
+            renderArtist(response.data);
+        })
+        .catch(function (error) {
+            // Code that will run on any errors from the server.
+            console.log(error);
+            console.error('ERROR', error)
+        })
+};
+
+function renderSong(songsFromServer) {
+    //songFromServer will be an Array of song data
+    const songDiv = document.querySelector('#songTableBody');
+    for (let song of songsFromServer) {
+        songDiv.innerHTML += `
+        <tr>
+            <td>${song.title}</td>
+            <td>${song.artist}</td>
+        </tr>
+    `;
+    }
+};
+
+function renderArtist(artistFromServer) {
+    // artistFromServer will be an Array of artist data
+    let artistDiv = document.querySelector('#artistTableBody');
+    for (let artist of artistFromServer) {
+        artistDiv.innerHTML += `
+        <tr>
+            <td>${artist.name}</td>
+            <td>${artist.born}</td>
+            <td>${artist.died}</td>
+        </tr>
+    `;
+    }
+};
 
 function submitSong(event) {
     //Post function for song
-
+    console.log('test');
     event.preventDefault();
     const titleElement = document.querySelector('#songTitle');
     const artistElement = document.querySelector('#songArtist');
-
+    
     const newSongCat = {
         title: titleElement.value,
         artist: artistElement.value,
@@ -78,15 +78,26 @@ function submitSong(event) {
 
     axios ({
         method: 'POST', 
-        url: '/artist', 
+        url: '/song', 
         data: newSongCat
     })
     .then((response) => {
+        //Clear fields
+        titleElement.value = '';
+        artistElement.value = '';
+
+        // Clear existing content in the song table body
+        const songTableBodyClear = document.querySelector('#songTableBody');
+        songTableBody.innerHTML = '';
+
+        //Get new data
         fetchSong();
 
     }).catch((error) => {
         console.error('ERROR', error);
     });    
+
+    form.reset();
 };
 
 function submitArtist(event) {
@@ -110,10 +121,25 @@ function submitArtist(event) {
         data: newArtistCat
     })
     .then((response) => {
-    //GET new data
-    fetchArtist();
+        //Clear fields
+        nameElement.value = '';
+        birthElement.value = '';
+        deathElement.value = '';
+
+        // Clear DOM
+        const artistTableBodyClear = document.querySelector('#artistTableBody');
+        artistTableBodyClear.innerHTML = '';
+
+        //Get new data
+        fetchArtist();
+
     }).catch((error) => {
         console.error('ERROR', error);
        });
 
+       FormData.rest
 };
+
+
+fetchSong();
+fetchArtist();
